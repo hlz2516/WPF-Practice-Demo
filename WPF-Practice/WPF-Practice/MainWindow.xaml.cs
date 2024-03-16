0c5f36ba90e43b4ebb8edec1bf98f2b699e71adf
hlz2516 <714251494@qq.com>
2024-03-16 23:32:42 +0800

    第一期：整体布局

--- WPF-Practice/WPF-Practice/MainWindow.xaml.cs
+++ WPF-Practice/WPF-Practice/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_Practice
 {
@@ -20,43 +8,9 @@ namespace WPF_Practice
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static List<string> Menu { get; set; }
-
         public MainWindow()
         {
             InitializeComponent();
-            //数据源
-            //Menu = new List<string>();
-            //Menu.Add("布局设置");
-            //Menu.Add("背景切换");
-            //准备binding
-            //Binding binding = new Binding();
-            //binding.Source = Menu;
-            //binding.Path = new PropertyPath(".");
-            ////使用binding连接源与目标
-            //BindingOperations.SetBinding(this.tc, TabControl.ItemsSourceProperty, binding);
-            //this.tc.DataContext = Menu;
-            tc.AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonClicked));
-            tc.AddHandler(BackgroundSetup.SetBackgroundImageEvent, new EventHandler<SetBackgroundImageEventArgs>(ApplyBackground));
-        }
-
-        private void ApplyBackground(object sender, SetBackgroundImageEventArgs e)
-        {
-            this.Background = new ImageBrush { ImageSource = e.BackgroundImage.Source };
-        }
-
-        private void ButtonClicked(object sender, RoutedEventArgs e)
-        {
-            //System.Diagnostics.Debug.WriteLine($"{(e.OriginalSource as FrameworkElement).Name}");
-            string clickedName = (e.OriginalSource as FrameworkElement).Name;
-            if (clickedName == "side_mode")
-            {
-                tc.TabStripPlacement = Dock.Left;
-            }
-            else if (clickedName == "top_mode")
-            {
-                tc.TabStripPlacement = Dock.Top;
-            }
         }
     }
 }
